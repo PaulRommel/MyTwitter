93293d4ce347ba3363a96da43cebf8d8293ed55d
Pavel Popov <pavelpopov@MacBook-Pro-Pavel.local>
2025-05-27 16:56:40 +0300

    Add Custom Tweet Cells
    Twitter - Custom Tweet Cells (Ep 8)

--- MyTwitter/Model/HomeDatasource.swift
+++ MyTwitter/Model/HomeDatasource.swift
@@ -19,12 +19,14 @@ class HomeDatasource: Datasource {
         return [brianUser, rayUser, kindleCourseUser]
     }()
     
+    let tweets = ["tweet1", "tweet2"]
+    
     override func headerClasses() -> [DatasourceCell.Type]? {
         return [UserHeader.self]
     }
     
     override func cellClasses() -> [DatasourceCell.Type] {
-        return [UserCell.self]
+        return [UserCell.self, TweetCell.self]
     }
     
     override func footerClasses() -> [DatasourceCell.Type]? {
@@ -35,7 +37,14 @@ class HomeDatasource: Datasource {
         return users[indexPath.item]
     }
     
+    override func numberOfSections() -> Int {
+        return 2
+    }
+    
     override func numberOfItems(_ section: Int) -> Int {
+        if section == 1 {
+            return tweets.count
+        }
         return users.count
     }
 }
