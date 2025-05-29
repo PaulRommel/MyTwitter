590a09c3d1b3cb96e0c2801ca23ab301e9fb9733
Pavel Popov <pavelpopov@MacBook-Pro-Pavel.local>
2025-05-29 15:32:55 +0300

    Add UIStackView Landscape and Device Sizing Support
    Twitter - UIStackView Landscape and Device Sizing Support (Ep 10)

--- MyTwitter/View/TweetCell.swift
+++ MyTwitter/View/TweetCell.swift
@@ -29,8 +29,6 @@ class TweetCell: DatasourceCell {
     
     let messageTextView: UITextView = {
         let tv = UITextView()
-        tv.text = "SOME SAMPLE TEXT"
-        tv.backgroundColor = .yellow
         return tv
     }()
     
@@ -40,6 +38,30 @@ class TweetCell: DatasourceCell {
         imageView.layer.cornerRadius = 5
         imageView.clipsToBounds = true
         return imageView
+    }()
+    
+    let replyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName:"arrowshape.turn.up.left.fill").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let retweetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName:"arrow.triangle.2.circlepath").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName:"hand.thumbsup.fill").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let directMessageButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName:"message.fill").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
     }()
     
     override func setupViews() {
@@ -56,5 +78,34 @@ class TweetCell: DatasourceCell {
         profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
         
         messageTextView.anchor(topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 4, leftConstant: 4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        setupBottomButtons()
+    }
+    
+    private func setupBottomButtons() {
+        let replyButtonContainerView = UIView()
+        let retweetButtonContainerView = UIView()
+        let likeButtonContainerView = UIView()
+        let directMessageButtonContainerView = UIView()
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [replyButtonContainerView, retweetButtonContainerView, likeButtonContainerView, directMessageButtonContainerView])
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        
+        addSubview(buttonStackView)
+        buttonStackView.anchor(nil, left: messageTextView.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        addSubview(replyButton)
+        addSubview(retweetButton)
+        addSubview(likeButton)
+        addSubview(directMessageButton)
+        
+        replyButton.anchor(replyButtonContainerView.topAnchor, left: replyButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        retweetButton.anchor(retweetButtonContainerView.topAnchor, left: retweetButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        likeButton.anchor(likeButtonContainerView.topAnchor, left: likeButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        directMessageButton.anchor(directMessageButtonContainerView.topAnchor, left: directMessageButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
     }
 }
